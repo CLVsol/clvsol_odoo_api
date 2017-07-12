72ef4eb0b68d8cfd31fac51aac7069fd9e467ac9
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2017-07-12 16:52:32 +0000

    [IMP] clv_address.py

--- clv_address.py
+++ clv_address.py
@@ -202,13 +202,15 @@ def myo_address_export_sqlite(client, args, db_path, table_name):
 
 def clv_address_import_sqlite(
         client, args, db_path, table_name, global_tag_table_name, category_table_name,
-        res_country_table_name, res_country_state_table_name, l10n_br_base_city_table_name
+        res_country_table_name, res_country_state_table_name, l10n_br_base_city_table_name,
+        res_users_table_name
 ):
 
     address_model = client.model('clv.address')
     res_country_model = client.model('res.country')
     res_country_state_model = client.model('res.country.state')
     l10n_br_base_city_model = client.model('l10n_br_base.city')
+    hr_employee_model = client.model('hr.employee')
 
     conn = sqlite3.connect(db_path)
     conn.row_factory = sqlite3.Row
@@ -358,6 +360,23 @@ def clv_address_import_sqlite(
         l10n_br_base_city_browse = l10n_br_base_city_model.browse([('ibge_code', '=', l10n_br_city_ibge_code), ])
         new_l10n_br_city_id_id = l10n_br_base_city_browse.id[0]
 
+        employee_id = False
+        cursor2.execute(
+            '''
+            SELECT name
+            FROM ''' + res_users_table_name + '''
+            WHERE id = ?;''',
+            (row['user_id'],
+             )
+        )
+        user_name = cursor2.fetchone()
+        print('>>>>>>>>>>', user_name)
+        if user_name is not None:
+            user_name = user_name[0]
+            print('>>>>>>>>>>>>>>>', user_name)
+            hr_employee_browse = hr_employee_model.browse([('name', '=', user_name), ])
+            employee_id = hr_employee_browse.id[0]
+
         values = {
             'global_tag_ids': new_tag_ids,
             'category_ids': new_category_ids,
@@ -365,7 +384,7 @@ def clv_address_import_sqlite(
             # 'alias': row['alias'],
             'code': row['code'],
             # 'random_field': row['random_field'],
-            # 'user_id': row['user_id'],
+            'employee_id': employee_id,
             'zip': row['zip'],
             'country_id': new_country_id,
             'state_id': new_state_id,
