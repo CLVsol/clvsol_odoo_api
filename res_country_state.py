7954b108c681fc52e72214b7919ebdc02ee765a9
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2017-07-30 17:28:07 +0000

    [IMP] res_country_state.py (ADD res_country_state_export_sqlite_10)

--- res_country_state.py
+++ res_country_state.py
@@ -84,3 +84,65 @@ def res_country_state_export_sqlite(client, args, db_path, table_name):
     print()
     print('--> res_country_state_count: ', res_country_state_count)
     print()
+
+
+def res_country_state_export_sqlite_10(client, args, db_path, table_name):
+
+    conn = sqlite3.connect(db_path)
+    conn.text_factory = str
+
+    cursor = conn.cursor()
+    try:
+        cursor.execute('''DROP TABLE ''' + table_name + ''';''')
+    except Exception as e:
+        print('------->', e)
+    cursor.execute(
+        '''
+        CREATE TABLE ''' + table_name + ''' (
+            id INTEGER NOT NULL PRIMARY KEY,
+            name,
+            code,
+            country_id,
+            ibge_code,
+            new_id INTEGER
+            );
+        '''
+    )
+
+    # client.context = {'active_test': False}
+    res_country_state_model = client.model('res.country.state')
+    res_country_state_browse = res_country_state_model.browse(args)
+
+    res_country_state_count = 0
+    for res_country_state_reg in res_country_state_browse:
+        res_country_state_count += 1
+
+        print(res_country_state_count, res_country_state_reg.id, res_country_state_reg.name.encode("utf-8"))
+
+        country_id = None
+        if res_country_state_reg.country_id:
+            country_id = res_country_state_reg.country_id.id
+
+        cursor.execute('''
+            INSERT INTO ''' + table_name + '''(
+                id,
+                name,
+                code,
+                country_id,
+                ibge_code
+                )
+            VALUES(?,?,?,?,?)
+            ''', (res_country_state_reg.id,
+                  res_country_state_reg.name,
+                  res_country_state_reg.code,
+                  country_id,
+                  res_country_state_reg.ibge_code,
+                  )
+        )
+
+    conn.commit()
+    conn.close()
+
+    print()
+    print('--> res_country_state_count: ', res_country_state_count)
+    print()
