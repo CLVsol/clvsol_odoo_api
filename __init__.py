46f6fd78de9c01944637035d5e6214ac96ffb1c8
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2017-07-31 18:28:06 +0000

    [ADD] hr_job.py

--- __init__.py
+++ __init__.py
@@ -22,6 +22,7 @@
 from hr_department_history import *
 from hr_employee import *
 from hr_employee_history import *
+from hr_job import *
 from hr_job_history import *
 from ir_sequence import *
 from l10n_br_base_city import *
