8c23220f1e0a9573b7a571ef6a4cfcf76aa36068
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2017-08-06 19:35:24 +0000

    [IMP] clv_lab_test_request.py (ADD clv_lab_test_request_import_sqlite)

--- clv_lab_test_request.py
+++ clv_lab_test_request.py
@@ -23,6 +23,8 @@ from __future__ import print_function
 
 import sqlite3
 
+from clv_history_marker import *
+
 
 def myo_lab_test_request_export_sqlite(client, args, db_path, table_name):
 
@@ -118,6 +120,126 @@ def myo_lab_test_request_export_sqlite(client, args, db_path, table_name):
                   lab_test_request_reg.state,
                   lab_test_request_reg.active,
                   )
+        )
+
+    conn.commit()
+    conn.close()
+
+    print()
+    print('--> lab_test_request_count: ', lab_test_request_count)
+
+
+def clv_lab_test_request_import_sqlite(
+        client, lab_test_request_args, db_path, table_name, lab_test_type_table_name,
+        person_table_name, res_users_table_name, history_marker_name
+):
+
+    # history_marker_id = clv_history_marker_get_id(client, history_marker_name)
+
+    lab_test_request_model = client.model('clv.lab_test.request')
+    lab_test_type_model = client.model('clv.lab_test.type')
+    # hr_employee_model = client.model('hr.employee')
+
+    conn = sqlite3.connect(db_path)
+    conn.row_factory = sqlite3.Row
+
+    cursor = conn.cursor()
+
+    cursor2 = conn.cursor()
+
+    lab_test_request_count = 0
+
+    data = cursor.execute(
+        '''
+        SELECT
+            id,
+            name,
+            lab_test_type_id,
+            date_requested,
+            patient_id,
+            survey_user_input_id,
+            person_user_id,
+            employee_id,
+            date_received,
+            state,
+            active,
+            new_id
+        FROM ''' + table_name + ''';
+        '''
+    )
+
+    print(data)
+    print([field[0] for field in cursor.description])
+    for row in cursor:
+        lab_test_request_count += 1
+
+        print(lab_test_request_count, row['id'], row['name'])
+
+        patient_id = False
+        if row['patient_id']:
+
+            patient_id = row['patient_id']
+
+            cursor2.execute(
+                '''
+                SELECT new_id
+                FROM ''' + person_table_name + '''
+                WHERE id = ?;''',
+                (patient_id,
+                 )
+            )
+            patient_id = cursor2.fetchone()[0]
+
+        lab_test_type_id = False
+        lab_test_type_ids = []
+        if row['lab_test_type_id']:
+
+            lab_test_type_id = row['lab_test_type_id']
+
+            cursor2.execute(
+                '''
+                SELECT code
+                FROM ''' + lab_test_type_table_name + '''
+                WHERE id = ?;''',
+                (lab_test_type_id,
+                 )
+            )
+            lab_test_type_code = cursor2.fetchone()[0]
+
+            new_test_type_id = lab_test_type_model.search([
+                ('code', '=', lab_test_type_code),
+            ])[0]
+            lab_test_type_ids.append((4, new_test_type_id))
+
+        previous_state = row['state']
+        if previous_state == 'draft':
+            state = 'draft'
+        if previous_state == 'tested':
+            state = 'tested'
+        if previous_state == 'canceled':
+            state = 'cancelled'
+
+        values = {
+            'code': row['name'],
+            'patient_id': patient_id,
+            'date_requested': row['date_requested'],
+            'lab_test_type_ids': lab_test_type_ids,
+            'state': state,
+            'active': row['active'],
+            'active_log': True,
+            # 'employee_id': employee_id,
+            # 'history_marker_id': history_marker_id,
+        }
+        lab_test_request_id = lab_test_request_model.create(values).id
+
+        cursor2.execute(
+            '''
+           UPDATE ''' + table_name + '''
+           SET new_id = ?
+           WHERE id = ?;''',
+            (lab_test_request_id,
+             row['id']
+             )
         )
 
     conn.commit()
