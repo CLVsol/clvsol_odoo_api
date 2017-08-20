3cca0cd1633157ecdb9dc617d8d46101b1cd3ce2
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2017-08-20 16:37:15 +0000

    [ADD] clv_person_history_log.py

--- __init__.py
+++ __init__.py
@@ -60,5 +60,6 @@ from clv_person_address_history_log import *
 from clv_person_address_role import *
 from clv_person_category import *
 from clv_person_history import *
+from clv_person_history_log import *
 from clv_person_log import *
 from clv_survey import *
